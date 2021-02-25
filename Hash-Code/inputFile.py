a12f64a1d77a064d19ac6d9e41e29a73bf6142e6
Morgane Ohlig <morganeohlig@eduroam-int-dhcp-98-71-24.ucl.ac.uk>
2021-02-25 19:15:31 +0000

    input taken

--- Hash-Code/inputFile.py
+++ Hash-Code/inputFile.py
@@ -1,3 +1,34 @@
 # Serves to take in data from the text files
 
-f = file.read("")
+file = open("a.txt")
+
+data = file.readlines() 
+intersectionList = []
+carsList = []
+listInfo = []
+
+intersections = 0
+cars = 0
+
+for index in range(len(data)):
+    line = data[index]
+    line = line.strip("\n")
+    if index == 0:
+        listInfo = line.split(" ")
+        intersections = int(listInfo[2])
+        cars = int(listInfo[3])
+        bonus = int(listInfo[4])
+    if ( 1 <= index <= intersections):
+        intersectionList.append(line)
+    if ( intersections < index <= intersections + cars):
+        carsList.append(line)
+
+
+
+# listInfo = (firstLine.strip("\n")).split(" ")
+print(listInfo) # DEBUGGING
+print(intersections) # DEBUGGING
+print(cars) # DEBUGGING
+print("---")
+print(intersectionList)
+print(carsList)
