4cbea8f70084c72a6c35be0078013733933c4b5e
Morgane Ohlig <morganeohlig@eduroam-int-dhcp-98-71-24.ucl.ac.uk>
2021-02-27 21:20:53 +0000

    example of interactions betweens files

--- StreetCar2021/inter_try.py
+++ StreetCar2021/inter_try.py
@@ -7,3 +7,17 @@ str(street.start) + " ",
 str(street.end) + " ",
 str(street.name) + " ",
 str(street.length))
+
+apple = street
+
+print(str(apple) + " ",
+str(apple.start) + " ",
+str(apple.end) + " ",
+str(apple.name) + " ",
+str(apple.length))
+
+print(str(street) + " ",
+str(street.start) + " ",
+str(street.end) + " ",
+str(street.name) + " ",
+str(street.length))
