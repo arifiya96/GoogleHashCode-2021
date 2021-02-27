1cde2e46e29f6d212451ff5156ddb293cc431e61
Morgane Ohlig <morganeohlig@eduroam-int-dhcp-98-71-24.ucl.ac.uk>
2021-02-27 18:38:50 +0000

    created separate file which can interoperate with main file

---
+++ StreetCar2021/inter_try.py
@@ -0,0 +1,9 @@
+from Traffic_lights import Street
+
+street = Street(0,1,"rue de paris", 4)
+
+print(str(street) + " ",
+str(street.start) + " ",
+str(street.end) + " ",
+str(street.name) + " ",
+str(street.length))
